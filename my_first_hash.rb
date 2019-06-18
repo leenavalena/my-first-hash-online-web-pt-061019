fc6c6007c6a7c2d8162b3f26ecdfe84aa39bcb4e
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-18 00:10:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
 def my_hash
   my_hash = {"name" => "Valena", "height" => "5'4", "weight" => "106 lb"}
   
-  
-  
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of 
 
   
